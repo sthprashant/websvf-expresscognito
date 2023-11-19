addf7010eadf379586754236dba29f6271e51cc2
Prashant Shrestha <31754379+sthprashant@users.noreply.github.com>
2023-11-19 14:14:17 +1100

    Update Userpool.js

--- Userpool.js
+++ Userpool.js
@@ -1,8 +1,8 @@
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 
 const poolData = {
-    UserPoolId: 'ap-southeast-2_Te0NecEf9',
-    ClientId: '6a9u8csgr2dijjbug816qmmvg3',
+    UserPoolId: '',
+    ClientId: '',
 }
 
 var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
